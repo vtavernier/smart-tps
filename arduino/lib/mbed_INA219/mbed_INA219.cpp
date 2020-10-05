189cf070b845f90533e925e3cdbc8f558fdfc511
Vincent Tavernier <vince.tavernier@gmail.com>
2020-10-06 01:00:27 +0200

    First somewhat working version

---
+++ arduino/lib/mbed_INA219/mbed_INA219.cpp
@@ -0,0 +1,47 @@
+#include "mbed_INA219.h"
+
+mbed_INA219::mbed_INA219(mbed::I2C &i2c, char addr)
+    : i2c_(i2c), addr_(addr), last_reg_(-1) {
+	write(INA219_REG_CALIBRATION, 4096);
+	write(INA219_REG_CONFIG, INA219_CONFIG_BVOLTAGERANGE_32V |
+				     INA219_CONFIG_GAIN_8_320MV |
+				     INA219_CONFIG_BADCRES_12BIT |
+				     INA219_CONFIG_SADCRES_12BIT_2S_1060US |
+				     INA219_CONFIG_MODE_SVOLT_CONTINUOUS);
+}
+
+void mbed_INA219::write(char addr, uint16_t val) {
+	char cmd[3];
+
+	cmd[0] = addr;
+	// MSBFIRST
+	cmd[1] = (val & 0xFF00) >> 8;
+	cmd[2] = val & 0xFF;
+
+	i2c_.write(addr_ << 1, cmd, sizeof(cmd));
+}
+
+int16_t mbed_INA219::read(char addr) {
+	if (last_reg_ != addr) {
+		// Initiate read
+		i2c_.write(addr_ << 1, &addr, sizeof(addr));
+		last_reg_ = addr;
+	}
+
+	// Read 2 bytes
+	char val[2] = {0};
+	if (i2c_.read(addr_ << 1, val, sizeof(val)) == 0)
+		return (val[0] << 8) | val[1];
+	else
+		return INT16_MAX;
+}
+
+float mbed_INA219::get_current() {
+	auto val = read(INA219_REG_CURRENT);
+
+	// Check for failed reads
+	if (val == INT16_MAX)
+		return MAXFLOAT;
+
+	return val / 10.f;
+}
--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,4 +1,4 @@
-#include <Adafruit_INA219.h>
+//#include <Adafruit_INA219.h>
 #include <Arduino.h>
 #include <SFE_MicroOLED.h>
 
@@ -8,34 +8,44 @@ const int OLED_DC = 8;
 
 MicroOLED oled(OLED_RST, OLED_DC, OLED_CS);
 
-const int LED_PINR = LEDR;
-const int LED_PING = LEDG;
-const int LED_PINB = LEDB;
-
 const int LOAD_SIM = 2;
 
-Adafruit_INA219 ina219(0x41);
+#include <mbed/mbed.h>
+mbed::PwmOut pinLoadSim(digitalPinToPinName(LOAD_SIM));
+mbed::PwmOut pinLedR(digitalPinToPinName(LEDR));
+mbed::PwmOut pinLedG(digitalPinToPinName(LEDG));
+mbed::PwmOut pinLedB(digitalPinToPinName(LEDB));
+
+#include <mbed_INA219.h>
+
+mbed::I2C i2c(digitalPinToPinName(PIN_WIRE_SDA),
+	      digitalPinToPinName(PIN_WIRE_SCL));
+
+#define INA219_ADDR 0x41
+mbed_INA219 *ina219;
 
 void setup() {
-	// Serial
-	Serial.begin(115200);
+	// Set frequency on the PWM pin
+	pinLoadSim.period_us(6667);
+	pinLoadSim.write(0.5f);
 
-	// Initialize INA219
-	if (!ina219.begin()) {
-		Serial.println("Failed to find INA219");
-		while (1) delay(100);
+	// Setup LEDS
+	pinLedR.write(1.0f);
+	pinLedG.write(1.0f);
+	pinLedB.write(1.0f);
+	for (auto *pin : {&pinLedR, &pinLedG, &pinLedB}) {
+		pin->period_ms(10);
 	}
 
-	// Initialize load output
-	pinMode(LOAD_SIM, OUTPUT);
+	pinLedR.write(0.0f);
 
-	// Initialize RGB LED
-	pinMode(LED_PINR, OUTPUT);
-	pinMode(LED_PING, OUTPUT);
-	pinMode(LED_PINB, OUTPUT);
-	digitalWrite(LED_PINR, HIGH);
-	digitalWrite(LED_PING, HIGH);
-	digitalWrite(LED_PINB, HIGH);
+	// Set I2C bus frequency
+	// TODO: 1MHz on PCB?
+	i2c.frequency(400000);
+	ina219 = new mbed_INA219(i2c, INA219_ADDR);
+
+	pinLedR.write(1.0f);
+	pinLedB.write(0.0f);
 
 	// Initialize, clear screen
 	oled.begin();
@@ -43,51 +53,190 @@ void setup() {
 	oled.clear(PAGE);
 	oled.clear(ALL);
 	oled.display();
+
+	pinLedB.write(1.0f);
+	pinLedG.write(0.0f);
 }
 
-uint32_t counter = 0;
-float avgbuf[40];
-size_t avgbufidx = 0;
+struct Sample {
+	unsigned long t;
+	float i;
+};
+
+const size_t SAMPLE_COUNT = 100;
+typedef Sample SampleBuffer[SAMPLE_COUNT];
+
+SampleBuffer samples = {0};
+size_t samples_idx = 0;
+unsigned long falling_edges[SAMPLE_COUNT] = {0};
+unsigned long rising_edges[SAMPLE_COUNT] = {0};
+
+
+const size_t STAT_COUNT = 10;
+float stat_freq = 0.f;
+float stat_duty_cycle_samples[STAT_COUNT] = {0};
+size_t stat_duty_cycle_ptr = 0;
+
+/**
+ * Set the current load status
+ *
+ * @param pwm Pulse width of the load
+ */
+void setLoad(float pwm) {
+	pinLoadSim.write(pwm);
+	pinLedR.write(1.0f - pwm);
+}
+
+unsigned long mean_lastms = 0;
+unsigned long screen_lastms = 0;
+bool setaddr = true;
 
 void loop() {
-	counter++;
+	float expected = ((millis() / 10000) % 4) / 4.f;
+	// Update load LED
+	setLoad(expected);
 
-	uint32_t phase = (counter / 4) % 512;
-
-	// Hard blink
-	if (phase > 255) {
-		phase = 255;
-	} else {
-		phase = 0;
-	}
-
-	// Smooth blink
-	//phase = phase > 255 ? 512 - phase : phase;
-	//phase = (phase * phase) / 255;
-
-	// Update load simulator and red LED for status
-	analogWrite(LED_PINR, 255 - phase);
-	analogWrite(LOAD_SIM, phase);
-
-	delay(5);
+	pinLedG.write(1.0f);
+	pinLedB.write(0.0f);
 
 	// Update current average
-	avgbuf[avgbufidx] = ina219.getCurrent_mA();
-	avgbufidx = (avgbufidx + 1) % (sizeof(avgbuf) / sizeof(float));
+	auto val = ina219->get_current();
+	if (val != MAXFLOAT) {
+		auto &c = samples[samples_idx];
+		c.t = micros();
+		c.i = val;
 
-	if (counter % 20 == 0) {
+		// Dump to serial
+		// TODO: Remove later or make it async?
+		// SerialUSB.print(c.t);
+		// SerialUSB.print(",");
+		// SerialUSB.println(c.i);
+
+		// Update index
+		samples_idx =
+		    (samples_idx + 1) % (sizeof(samples) / sizeof(samples[0]));
+	}
+
+	pinLedB.write(1.0f);
+
+	unsigned long now = millis();
+
+	if (now - mean_lastms > 50) {
+		mean_lastms = now;
+
+		// First pass, determine max and min
+		float max = -10000.f, min = 10000.f;
+		for (auto &s : samples) {
+			if (s.i > max) max = s.i;
+			if (s.i < min) min = s.i;
+		}
+
+		// Currently no current, skip it
+		if (max < 5.0f) {
+			return;
+		}
+
+		float mid = (max + min) / 2.0;
+
+		// Second pass, determine edges
+		memset(falling_edges, 0, sizeof(falling_edges));
+		memset(rising_edges, 0, sizeof(rising_edges));
+
+		bool high = samples[0].i > mid;
+		size_t falling_ptr = 0;
+		size_t rising_ptr = 0;
+		unsigned long last_t = samples[0].t;
+
+		for (size_t i = 0; i < SAMPLE_COUNT; ++i) {
+			auto &s = samples[(i + samples_idx) % SAMPLE_COUNT];
+
+			if (high && s.i < mid) {
+				// Falling edge
+				high = false;
+				falling_edges[falling_ptr++] =
+				    (last_t + s.t) / 2;
+			} else if (!high && s.i > mid) {
+				// Rising edge
+				high = true;
+				rising_edges[rising_ptr++] = (last_t + s.t) / 2;
+			}
+
+			last_t = s.t;
+		}
+
+		// Third pass: compute period and duty cycle
+		size_t max_ptr =
+		    falling_ptr < rising_ptr ? falling_ptr : rising_ptr;
+
+		unsigned long falling_sum = 0, rising_sum = 0,
+			      dt_duration_sum = 0;
+		unsigned long valid = 0;
+
+		for (size_t i = 1; i < max_ptr; ++i) {
+			if (falling_edges[i] > falling_edges[i - 1] &&
+			    rising_edges[i] > rising_edges[i - 1]) {
+				// Frequency computation
+				falling_sum +=
+				    falling_edges[i] - falling_edges[i - 1];
+				rising_sum +=
+				    rising_edges[i] - rising_edges[i - 1];
+
+				// Duty-cycle computation
+				if (falling_edges[i] < rising_edges[i]) {
+					dt_duration_sum += falling_edges[i] -
+							   rising_edges[i - 1];
+				} else {
+					dt_duration_sum += rising_edges[i] -
+							   falling_edges[i - 1];
+				}
+				valid++;
+			}
+		}
+
+		if (valid > 0) {
+			// Compute period from average of rising and falling
+			// edge distance
+			float period =
+			    (falling_sum + rising_sum) / (2.0f * valid);
+
+			// Compute duty cycle from this
+			float dtc = (100.0f * dt_duration_sum / valid) / period;
+
+			// Compute frequency
+			float freq = 1.0e6f / period;
+
+			// Update stats
+			stat_duty_cycle_samples[stat_duty_cycle_ptr] = dtc;
+			stat_duty_cycle_ptr = (stat_duty_cycle_ptr + 1) % STAT_COUNT;
+			stat_freq = freq;
+		}
+	}
+
+	if (now - screen_lastms > 250) {
+		screen_lastms = now;
+
 		oled.clear(PAGE);
-		float c = 0.0f;
-		for (size_t i = 0; i < sizeof(avgbuf) / sizeof(float); ++i)
-			c += avgbuf[i];
-		c /= sizeof(avgbuf) / sizeof(float);
 
 		oled.setCursor(0, 0);
-		if (c >= 0.0f) {
-			oled.print(" ");
-		}
-		oled.print(c);
-		oled.println(" mA");
+		oled.print(stat_freq);
+		oled.setCursor(oled.getFontWidth() * 6, 0);
+		oled.println("Hz");
+
+		float dtc = 0.0f;
+		for (size_t i = 0; i < STAT_COUNT; ++i)
+			dtc += stat_duty_cycle_samples[i];
+		dtc /= STAT_COUNT;
+
+		oled.print(dtc);
+		oled.setCursor(oled.getFontWidth() * 6, oled.getFontHeight());
+		oled.println("%");
+
+		oled.println();
+		oled.print(100.f * expected);
+		oled.setCursor(oled.getFontWidth() * 6,
+			       3 * oled.getFontHeight());
+		oled.println("%");
+
 		oled.display();
 	}
 }
